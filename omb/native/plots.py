de5aac4e2626e508536954d78b87961b1211c884
Alan Cueva <alan.cueva@dell.com>
2025-02-11 11:41:37 +0000

    New test cases

---
+++ omb/native/plots.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def set_label(row):
+    if row['Message_Size'] > 1000:
+        val = int(row['Message_Size'] / 1024)
+        return str( val ) + "KB"
+    else:
+        return str( int(row['Message_Size']) ) + "B"
+
+def plot_row(row):
+    df = pd.read_csv('result.csv')
+    grouped_df = df.groupby('Test_Case').mean().reset_index()
+    grouped_df['label'] = grouped_df.apply( set_label, axis=1 )
+    
+    plt.step(grouped_df['Test_Case'], grouped_df[row])
+    plt.xticks(ticks=grouped_df['Test_Case'], labels=grouped_df['label'], rotation=90)
+    plt.xlabel('Test Cases')
+    plt.ylabel(row)
+    plt.grid(True)
+    plt.savefig(row + '.png', bbox_inches='tight')
+    plt.clf()
+    print('Plot ' + row + '.png generated')
+
+plot_row('Latency_50')
+plot_row('Latency_75')
+plot_row('Latency_95')
+plot_row('Latency_99')
--- omb/native/report.py
+++ omb/native/report.py
@@ -13,12 +13,12 @@ report_data = {
     'Latency_75': [],
     'Latency_95': [],
     'Latency_99': [],
+    'Message_Size': [],
     'Sent_Messages': [],
     'Sent_Bytes': [],
     'Duration': [],
     'Throughout': []
 }
-#print('Test_Case,Latency_50,Latency_75,Latency_95,Latency_99,Sent_Messages,Sent_Bytes,Duration,Throughout')
 
 file_list = glob.glob(os.path.join(directory, extension))
 file_list.sort()
@@ -31,6 +31,7 @@ for file_path in file_list:
     report_data['Latency_75'].append( data.get('aggregatedPublishLatency75pct') )
     report_data['Latency_95'].append( data.get('aggregatedPublishLatency95pct') )
     report_data['Latency_99'].append( data.get('aggregatedPublishLatency99pct') )
+    report_data['Message_Size'].append( data.get('messageSize') )
     report_data['Sent_Messages'].append( data.get('totalMessagesSent') )
     
     Sent_Bytes = data.get('totalBytesSent')
@@ -41,5 +42,5 @@ for file_path in file_list:
     report_data['Throughout'].append( (Sent_Bytes*0.000001)/(Duration*60) )
 
 df = pd.DataFrame(report_data)
-df.to_csv('results.csv', index=False)
-print('Metrics written in results.csv')
+df.to_csv('result.csv', index=False)
+print('Metrics written in result.csv')
